05de20237d0a359267199271e3f2e60d50991bed
Dean Wan <deanwan14@gmail.com>
2024-03-27 20:29:49 +1100

    feat: add status code to header

--- pkg/grace/grace.go
+++ pkg/grace/grace.go
@@ -11,16 +11,17 @@ type PanicHandler struct {
 }
 
 // ServeHTTP handles the request by passing it to the real handler
-func (p *PanicHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (p *PanicHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	defer func() {
 		if r := recover(); r != nil {
 			// Dump stacktrace to logs
 			fmt.Println("stacktrace from panic: \n" + string(debug.Stack()))
-			fmt.Fprint(w, "<h1>Something went wrong!</h1>")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Something went wrong!"))
 			// Respond with status
 		}
 	}()
-	p.handler.ServeHTTP(w, r)
+	p.handler.ServeHTTP(w, req)
 }
 
 // NewPanicHandler constructs a new PanicHandler middleware handler
